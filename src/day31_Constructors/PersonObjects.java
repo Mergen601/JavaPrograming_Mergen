07f45d5b210b8fbcbe41abd728991cac85d880aa
Mergen601 <95243537+Mergen601@users.noreply.github.com>
2022-08-12 14:48:50 +0300

    new

---
+++ src/day31_Constructors/PersonObjects.java
@@ -0,0 +1,16 @@
+package day31_Constructors;
+
+public class PersonObjects
+{
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Fatma",'F',15);
+        Person person2 = new Person("Beyza",'F',18);
+        Person person3 = new Person("İhsan",'M',22);
+
+        System.out.println(person1);
+
+
+
+    }
+}
