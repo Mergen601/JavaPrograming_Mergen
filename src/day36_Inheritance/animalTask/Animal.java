5ba473a2396abfc4f94cecc733380ed255a0d94f
Mergen601 <95243537+Mergen601@users.noreply.github.com>
2022-09-01 10:05:42 +0300

    dasda

---
+++ src/day36_Inheritance/animalTask/Animal.java
@@ -0,0 +1,29 @@
+package day36_Inheritance.animalTask;
+
+public class Animal {
+
+    public String name;
+    public int age;
+
+
+
+
+
+    //Animal:
+    //name, breed, gender, size, age, color
+    //setInfo(), eat(), drink(), move(), sleep(), toString()
+    //Dog extends Animal:
+    //bark()
+    //Cat extends Animal:
+    //scratch()
+    //Tiger extends Animal:
+    //roar()
+    //Fish extends Animal:
+    //swim()
+    //...
+    //import vs extends:
+    //import: borrowing
+    //extends: owning
+    //inherit ->pass it through
+    //import -> let using
+}
