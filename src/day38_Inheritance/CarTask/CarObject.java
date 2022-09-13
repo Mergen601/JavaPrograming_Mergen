53f4f99fc72269e83b06492111c08ee43a3b24d4
Mergen601 <95243537+Mergen601@users.noreply.github.com>
2022-09-13 10:08:58 +0300

    ScrumTeam

---
+++ src/day38_Inheritance/CarTask/CarObject.java
@@ -0,0 +1,17 @@
+package day38_Inheritance.CarTask;
+
+public class CarObject {
+
+    public static void main(String[] args) {
+
+        Tesla tesla = new Tesla("T3",2018,48200,"Green");
+        tesla.autoPilot();
+
+
+
+    }
+
+
+
+
+}
---
+++ src/day39_Recap/MethodOverloading.java
@@ -0,0 +1,13 @@
+package day39_Recap;
+
+public class MethodOverloading {
+
+    private void method(){
+        System.out.println("Hello World");
+    }
+
+    private int method(int a) {
+        return a;
+    }
+
+}
